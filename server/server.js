9a69ff0db6a8b332b094142a793df17328a3f580
AceSpadee <148384493+AceSpadee@users.noreply.github.com>
2024-09-25 13:29:54 -0700

    testing backend

--- server/server.js
+++ server/server.js
@@ -40,13 +40,14 @@ const startApolloServer = async () => {
   );
 
   // If we're in production, serve client/dist as static assets.
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
-  }
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  //   });
+  // }
+
   // DB once open, start the server and listen to the ports.
   db.once("open", () => {
     console.log("MongoDB connected successfully");
